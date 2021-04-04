1b5b2354d836b63028ee29b25bf0d51ff99519e1
Lukas Robinson <lukas.robinson427@gmail.com>
2021-04-04 15:45:59 -0500

    progress

--- eda project - Lukas Robinson.R
+++ eda project - Lukas Robinson.R
@@ -6,33 +6,64 @@ library(ape)
 library(maps)
 library(sp)
 library(lubridate)
+library(sf)
+library(elevatr)
+library(USAboundaries)
 
 #list of plant species in united states
-list<-BIEN_list_country(country="United States")
+list <- BIEN_list_country(country = "United States")
 
 # three flowering plant species in Arizona -----------------------------------
 
-Encelia_farinosa<-BIEN_occurrence_species(species="Encelia farinosa")
+Encelia_farinosa <-
+  BIEN_occurrence_species(species = "Encelia farinosa")
 
-Rafinesquia_neomexicana<-BIEN_occurrence_species(species="Rafinesquia neomexicana")
+Rafinesquia_neomexicana <-
+  BIEN_occurrence_species(species = "Rafinesquia neomexicana")
 
-Phacelia_distans<-BIEN_occurrence_species(species="Phacelia distans")
+Phacelia_distans <-
+  BIEN_occurrence_species(species = "Phacelia distans")
 
-#filter of three dates 
+#all data
 
-Encelia_farinosa_date<-filter(Encelia_farinosa, year(date_collected)%in%c(2015,2005,1995))
+all_data <-
+  bind_rows(Encelia_farinosa,
+            Rafinesquia_neomexicana,
+            Phacelia_distans) %>% 
+  as_tibble() %>% 
+  transmute(
+    species = scrubbed_species_binomial,
+    latitude,
+    longitude,
+    date_collected,
+    year = as.factor(year(date_collected))
+  ) %>% 
+  filter(year %in% c(2015, 2005, 1995)) %>% 
+  st_as_sf(coords = c("longitude", "latitude"),crs = 4326) %>% 
+  print()
 
-Rafinesquia_neomexicana_date<-filter(Rafinesquia_neomexicana, year(date_collected)%in%c(2015,2005,1995))
+#map data
 
-Phacelia_distans_date<-filter(Phacelia_distans, year(date_collected)%in%c(2015,2005,1995))
+map_data("state") %>% 
+  filter(region == "arizona") %>% 
+  st_as_sf(coords = c("long", "lat"),crs = 4326)
 
-#plot data
+#us states data
 
-map('world',fill = T , col= "grey", bg = "light blue",xlim = c(-180,-20),ylim = c(-60,80))
-plot(Encelia_farinosa,col = "green",add = T)
+us_states() %>% 
+  filter(state_name %in% c("Arizona", "California")) %>% 
+  ggplot() + 
+  geom_sf() +
+  geom_sf(mapping = aes(color = species, shape = year),
+          data = all_data) +
+  facet_grid(year ~ species)
+
+#elevation point data
+
+all_data %>% 
+  get_elev_point()
 
 #Citation
-
 
 citation("BIEN")
 
